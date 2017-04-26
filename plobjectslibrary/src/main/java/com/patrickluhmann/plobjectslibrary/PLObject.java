370b742d63ae62ca360eec29429d639145e99e44
Patrick Luhmann <patrick.luhmann@gmail.com>
2017-04-26 11:04:05 -0700

    Add basic construction methods, updatePosition()
    The PLObject class represents a object with a size, position, velocity,
    and color. Of these, size is mandatory and the rest are optional, with
    default values. Unit tests are provided for the build methods.
    
    The method updatePosition() is implemented. It applies the given velocity
    to the object's position, based on a given time delta. Unit tests are
    provided for basic usage. Corner cases and extreme values are not covered.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

--- plobjectslibrary/src/main/java/com/patrickluhmann/plobjectslibrary/PLObject.java
+++ plobjectslibrary/src/main/java/com/patrickluhmann/plobjectslibrary/PLObject.java
@@ -1,19 +1,26 @@
 package com.patrickluhmann.plobjectslibrary;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 
 public class PLObject {
 	public static String getClassName() { return "PLObject"; }
+	private int sizeX;
+	private int sizeY;
 	private float posX;
 	private float posY;
 	private float velX;
 	private float velY;
+	private int color;
 
 	public static abstract class AbsPLOBuilder<C extends PLObject, B extends AbsPLOBuilder<C, B>> {
+		int sizeX = 0;
+		int sizeY = 0;
 		private float posX = 0;
 		private float posY = 0;
 		private float velX = 0;
 		private float velY = 0;
+		private int color = Color.WHITE;
 
 		@SuppressWarnings("unchecked")
 		public B posX(float val) {
@@ -39,6 +46,12 @@ public class PLObject {
 			return (B) this;
 		}
 
+		@SuppressWarnings("unchecked")
+		public B color(int val) {
+			this.color = val;
+			return (B) this;
+		}
+
 		public abstract C build();
 	}
 
@@ -49,15 +62,43 @@ public class PLObject {
 		}
 	}
 
-	public static AbsPLOBuilder<?, ?> builder() {
-		return new PLOBuilder();
+	// Builder interface
+	// Mandatory parameters:
+	// - sizeX and sizeY: the dimensions of the object.
+	public static AbsPLOBuilder<?, ?> builder(int sizeX, int sizeY) {
+		PLOBuilder temp = new PLOBuilder();
+		temp.sizeX = sizeX;
+		temp.sizeY = sizeY;
+		return temp;
 	}
 
 	protected PLObject(AbsPLOBuilder<?, ?> builder) {
+		sizeX = builder.sizeX;
+		sizeY = builder.sizeY;
 		posX = builder.posX;
 		posY = builder.posY;
 		velX = builder.velX;
 		velY = builder.velY;
+		color = builder.color;
+	}
+
+	public void getSize(int[] pos) {
+		pos[0] = sizeX;
+		pos[1] = sizeY;
+	}
+
+	public void getPosition(float[] pos) {
+		pos[0] = posX;
+		pos[1] = posY;
+	}
+
+	public void getVelocity(float[] pos) {
+		pos[0] = velX;
+		pos[1] = velY;
+	}
+
+	public int getColor() {
+		return color;
 	}
 
 	public void draw(Canvas canvas) {
@@ -65,6 +106,7 @@ public class PLObject {
 	}
 
 	public void updatePosition(float deltaT) {
-		// If this object has a velocity, update its position.
+		posX += (velX * deltaT);
+		posY += (velY * deltaT);
 	}
 }
--- plobjectslibrary/src/test/java/com/patrickluhmann/plobjectslibrary/PLObjectTest.java
+++ plobjectslibrary/src/test/java/com/patrickluhmann/plobjectslibrary/PLObjectTest.java
@@ -1,19 +1,151 @@
 package com.patrickluhmann.plobjectslibrary;
 
+import android.graphics.Color;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class PLObjectTest {
 	@Test
-	public void basic() throws Exception {
+	public void updatePosition_NoVelocity() {
+		PLObject obj = PLObject.builder(10, 10).posX(100).posY(200).velX(0).velY(0).build();
+		float deltaT = 0.01f;
+		float[] actFloats = new float[2];
+
+		obj.updatePosition(deltaT);
+		obj.getPosition(actFloats);
+		assertEquals(100, actFloats[0], 0.0d);
+		assertEquals(200, actFloats[1], 0.0d);
+	}
+
+	@Test
+	public void updatePosition_PositiveXVelocity() {
+		PLObject obj = PLObject.builder(10, 10).posX(100).posY(200).velX(20).velY(0).build();
+		float deltaT = 0.01f;
+		float[] actFloats = new float[2];
+
+		obj.updatePosition(deltaT);
+		obj.getPosition(actFloats);
+		assertEquals(100 + deltaT * 20, actFloats[0], 0.0d);
+		assertEquals(200, actFloats[1], 0.0d);
+	}
+
+	@Test
+	public void updatePosition_NegativeXVelocity() {
+		PLObject obj = PLObject.builder(10, 10).posX(100).posY(200).velX(-30).velY(0).build();
+		float deltaT = 0.01f;
+		float[] actFloats = new float[2];
+
+		obj.updatePosition(deltaT);
+		obj.getPosition(actFloats);
+		assertEquals(100 + deltaT * -30, actFloats[0], 0.0d);
+		assertEquals(200, actFloats[1], 0.0d);
+	}
+
+	@Test
+	public void updatePosition_PositiveYVelocity() {
+		PLObject obj = PLObject.builder(10, 10).posX(100).posY(200).velX(0).velY(40).build();
+		float deltaT = 0.01f;
+		float[] actFloats = new float[2];
+
+		obj.updatePosition(deltaT);
+		obj.getPosition(actFloats);
+		assertEquals(100, actFloats[0], 0.0d);
+		assertEquals(200 + deltaT * 40, actFloats[1], 0.0d);
+	}
+
+	@Test
+	public void updatePosition_NegativeYVelocity() {
+		PLObject obj = PLObject.builder(10, 10).posX(100).posY(200).velX(0).velY(-50).build();
+		float deltaT = 0.01f;
+		float[] actFloats = new float[2];
+
+		obj.updatePosition(deltaT);
+		obj.getPosition(actFloats);
+		assertEquals(100, actFloats[0], 0.0d);
+		assertEquals(200 + deltaT * -50, actFloats[1], 0.0d);
+	}
+
+	@Test
+	public void updatePosition_PositiveBothVelocity() {
+		PLObject obj = PLObject.builder(10, 10).posX(100).posY(200).velX(60).velY(40).build();
+		float deltaT = 0.01f;
+		float[] actFloats = new float[2];
+
+		obj.updatePosition(deltaT);
+		obj.getPosition(actFloats);
+		assertEquals(100 + deltaT * 60, actFloats[0], 0.0d);
+		assertEquals(200 + deltaT * 40, actFloats[1], 0.0d);
+	}
+
+	@Test
+	public void updatePosition_NegativeBothVelocity() {
+		PLObject obj = PLObject.builder(10, 10).posX(100).posY(200).velX(-70).velY(-50).build();
+		float deltaT = 0.01f;
+		float[] actFloats = new float[2];
+
+		obj.updatePosition(deltaT);
+		obj.getPosition(actFloats);
+		assertEquals(100 + deltaT * -70, actFloats[0], 0.0d);
+		assertEquals(200 + deltaT * -50, actFloats[1], 0.0d);
+	}
+
+	@Test
+	public void updatePosition_MixedVelocity() {
+		PLObject obj = PLObject.builder(10, 10).posX(100).posY(200).velX(-70).velY(50).build();
+		float deltaT = 0.01f;
+		float[] actFloats = new float[2];
+
+		obj.updatePosition(deltaT);
+		obj.getPosition(actFloats);
+		assertEquals(100 + deltaT * -70, actFloats[0], 0.0d);
+		assertEquals(200 + deltaT * 50, actFloats[1], 0.0d);
+	}
+
+	@Test
+	public void construction() throws Exception {
 		assertEquals("PLObject", PLObject.getClassName());
 
-		PLObject obj1 = PLObject.builder().posX(1).posY(2).velX(3).velY(4).build();
+		int[] actInts = new int[2];
+		float[] actFloats = new float[2];
+
+		// Don't specify any optional values
+		PLObject obj0 = PLObject.builder(1, 2).build();
+
+		obj0.getPosition(actFloats);
+		assertEquals(0, actFloats[0], 0.0d);
+		assertEquals(0, actFloats[1], 0.0d);
+
+		obj0.getVelocity(actFloats);
+		assertEquals(0, actFloats[0], 0.0d);
+		assertEquals(0, actFloats[1], 0.0d);
+
+		obj0.getSize(actInts);
+		assertEquals(1, actInts[0]);
+		assertEquals(2, actInts[1]);
+
+		assertEquals(Color.WHITE, obj0.getColor());
+
+		// Specify all values
+		PLObject obj1 = PLObject.builder(123, 456)
+			.color(Color.BLUE)
+			.posX(1).posY(2)
+			.velX(3).velY(4)
+			.build();
+
+		obj1.getPosition(actFloats);
+		assertEquals(1, actFloats[0], 0.0d);
+		assertEquals(2, actFloats[1], 0.0d);
+
+		obj1.getVelocity(actFloats);
+		assertEquals(3, actFloats[0], 0.0d);
+		assertEquals(4, actFloats[1], 0.0d);
+
+		obj1.getSize(actInts);
+		assertEquals(123, actInts[0]);
+		assertEquals(456, actInts[1]);
+
+		assertEquals(Color.BLUE, obj1.getColor());
 	}
 }
